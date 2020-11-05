78c5f11693eb4adb4418bc0051422d050254ab14
苏向东 <suxiangdong@zcool.com.cn>
2020-11-05 18:41:52 +0800

    尾递归

--- code/02/factorial.go
+++ code/02/factorial.go
@@ -7,3 +7,11 @@ func Factorial1(n int) int {
 	}
 	return n * Factorial1(n-1)
 }
+
+// 尾递归实现
+func Factorial2(n, r int) int {
+	if n == 0 {
+		return r
+	}
+	return Factorial2(n - 1, r * n)
+}
--- code/02/factorial_test.go
+++ code/02/factorial_test.go
@@ -1,6 +1,8 @@
 package code
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFactorial(t *testing.T) {
 	t.Parallel()
@@ -16,5 +18,47 @@ func TestFactorial(t *testing.T) {
 		if r := Factorial1(v.in); r != v.out {
 			t.Fatal("Factorial1 test fail")
 		}
+	}
+	for _, v := range datas {
+		if r := Factorial2(v.in, 1); r != v.out {
+			t.Fatal("Factorial2 test fail")
+		}
+	}
+}
+
+// 普通递归基准测试
+//
+//BenchmarkFact1_1-4      302576580                3.97 ns/op
+//BenchmarkFact1_2-4      93612594                12.4 ns/op
+//BenchmarkFact1_3-4      59797405                19.0 ns/op
+//BenchmarkFact1_4-4      33646185                36.0 ns/op
+//BenchmarkFact1_5-4      18706471                66.9 ns/op
+func BenchmarkFact1_1(b *testing.B) {benchmarkFactorial1(1, b)}
+func BenchmarkFact1_2(b *testing.B) {benchmarkFactorial1(5, b)}
+func BenchmarkFact1_3(b *testing.B) {benchmarkFactorial1(8, b)}
+func BenchmarkFact1_4(b *testing.B) {benchmarkFactorial1(10, b)}
+func BenchmarkFact1_5(b *testing.B) {benchmarkFactorial1(20, b)}
+
+// 尾递归基准测试
+//BenchmarkFact2_1-4      308755232                3.84 ns/op
+//BenchmarkFact2_2-4      100000000               11.9 ns/op
+//BenchmarkFact2_3-4      66315529                17.4 ns/op
+//BenchmarkFact2_4-4      46351069                24.9 ns/op
+//BenchmarkFact2_5-4      22874569                52.9 ns/op
+func BenchmarkFact2_1(b *testing.B) {benchmarkFactorial2(1, b)}
+func BenchmarkFact2_2(b *testing.B) {benchmarkFactorial2(5, b)}
+func BenchmarkFact2_3(b *testing.B) {benchmarkFactorial2(8, b)}
+func BenchmarkFact2_4(b *testing.B) {benchmarkFactorial2(10, b)}
+func BenchmarkFact2_5(b *testing.B) {benchmarkFactorial2(20, b)}
+
+func benchmarkFactorial1(n int, b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Factorial1(n)
+	}
+}
+
+func benchmarkFactorial2(n int, b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Factorial2(n, 1)
 	}
 }
