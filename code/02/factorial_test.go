e042b4a3106cfda0b24a9dba0a3b0724a1c7830c
苏向东 <suxiangdong@zcool.com.cn>
2020-11-06 17:41:09 +0800

    尾递归

--- code/02/factorial_test.go
+++ code/02/factorial_test.go
@@ -28,11 +28,11 @@ func TestFactorial(t *testing.T) {
 
 // 普通递归基准测试
 //
-//BenchmarkFact1_1-4      302576580                3.97 ns/op
-//BenchmarkFact1_2-4      93612594                12.4 ns/op
-//BenchmarkFact1_3-4      59797405                19.0 ns/op
-//BenchmarkFact1_4-4      33646185                36.0 ns/op
-//BenchmarkFact1_5-4      18706471                66.9 ns/op
+//BenchmarkFact1_1-4      295683223                3.99 ns/op            0 B/op          0 allocs/op
+//BenchmarkFact1_2-4      96102536                12.6 ns/op             0 B/op          0 allocs/op
+//BenchmarkFact1_3-4      61382451                19.0 ns/op             0 B/op          0 allocs/op
+//BenchmarkFact1_4-4      33726433                35.5 ns/op             0 B/op          0 allocs/op
+//BenchmarkFact1_5-4      18646712                64.9 ns/op             0 B/op          0 allocs/op
 func BenchmarkFact1_1(b *testing.B) {benchmarkFactorial1(1, b)}
 func BenchmarkFact1_2(b *testing.B) {benchmarkFactorial1(5, b)}
 func BenchmarkFact1_3(b *testing.B) {benchmarkFactorial1(8, b)}
@@ -40,11 +40,11 @@ func BenchmarkFact1_4(b *testing.B) {benchmarkFactorial1(10, b)}
 func BenchmarkFact1_5(b *testing.B) {benchmarkFactorial1(20, b)}
 
 // 尾递归基准测试
-//BenchmarkFact2_1-4      308755232                3.84 ns/op
-//BenchmarkFact2_2-4      100000000               11.9 ns/op
-//BenchmarkFact2_3-4      66315529                17.4 ns/op
-//BenchmarkFact2_4-4      46351069                24.9 ns/op
-//BenchmarkFact2_5-4      22874569                52.9 ns/op
+//BenchmarkFact2_1-4      307067715                3.93 ns/op            0 B/op          0 allocs/op
+//BenchmarkFact2_2-4      99202470                11.9 ns/op             0 B/op          0 allocs/op
+//BenchmarkFact2_3-4      68360604                17.7 ns/op             0 B/op          0 allocs/op
+//BenchmarkFact2_4-4      48932998                24.7 ns/op             0 B/op          0 allocs/op
+//BenchmarkFact2_5-4      22987033                52.8 ns/op             0 B/op          0 allocs/op
 func BenchmarkFact2_1(b *testing.B) {benchmarkFactorial2(1, b)}
 func BenchmarkFact2_2(b *testing.B) {benchmarkFactorial2(5, b)}
 func BenchmarkFact2_3(b *testing.B) {benchmarkFactorial2(8, b)}
@@ -52,12 +52,14 @@ func BenchmarkFact2_4(b *testing.B) {benchmarkFactorial2(10, b)}
 func BenchmarkFact2_5(b *testing.B) {benchmarkFactorial2(20, b)}
 
 func benchmarkFactorial1(n int, b *testing.B) {
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		Factorial1(n)
 	}
 }
 
 func benchmarkFactorial2(n int, b *testing.B) {
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		Factorial2(n, 1)
 	}
