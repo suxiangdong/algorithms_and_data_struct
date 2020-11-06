3715750763062faeb00a090a12fac2db7568abf7
苏向东 <suxiangdong@zcool.com.cn>
2020-11-06 18:52:10 +0800

    消除尾递归

--- code/02/factorial.go
+++ code/02/factorial.go
@@ -1,5 +1,9 @@
 package code
 
+import (
+	stack2 "github.com/golang-collections/collections/stack"
+)
+
 // 阶乘的基本递归实现
 func Factorial1(n int) int {
 	if n == 0 {
@@ -14,4 +18,30 @@ func Factorial2(n, r int) int {
 		return r
 	}
 	return Factorial2(n - 1, r * n)
+}
+
+// 手工压栈、非递归实现
+func Factorial3(n int) int {
+	res := 1
+	stack := stack2.New()
+	for n > 0 {
+		stack.Push(n)
+		n--
+	}
+	for stack.Len() > 0 {
+		v := stack.Pop()
+		// 确定v一定是int，否则可用switch v.(type)处理
+		res *= v.(int)
+	}
+	return res
+}
+
+// 循环实现
+func Factorial4(n int) int {
+	res := 1
+	for n > 0 {
+		res *= n
+		n--
+	}
+	return res
 }
--- code/02/factorial_test.go
+++ code/02/factorial_test.go
@@ -13,7 +13,6 @@ func TestFactorial(t *testing.T) {
 		{in: 10, out: 3628800},
 		{in: 0, out: 1},
 	}
-
 	for _, v := range datas {
 		if r := Factorial1(v.in); r != v.out {
 			t.Fatal("Factorial1 test fail")
@@ -22,6 +21,16 @@ func TestFactorial(t *testing.T) {
 	for _, v := range datas {
 		if r := Factorial2(v.in, 1); r != v.out {
 			t.Fatal("Factorial2 test fail")
+		}
+	}
+	for _, v := range datas {
+		if r := Factorial3(v.in); r != v.out {
+			t.Fatal("Factorial3 test fail")
+		}
+	}
+	for _, v := range datas {
+		if r := Factorial4(v.in); r != v.out {
+			t.Fatal("Factorial4 test fail")
 		}
 	}
 }
@@ -51,6 +60,30 @@ func BenchmarkFact2_3(b *testing.B) {benchmarkFactorial2(8, b)}
 func BenchmarkFact2_4(b *testing.B) {benchmarkFactorial2(10, b)}
 func BenchmarkFact2_5(b *testing.B) {benchmarkFactorial2(20, b)}
 
+// 手动压栈
+//BenchmarkFact3_1-4      22467722                47.4 ns/op            40 B/op          2 allocs/op
+//BenchmarkFact3_2-4       5100942               236 ns/op             200 B/op         10 allocs/op
+//BenchmarkFact3_3-4       3214581               375 ns/op             320 B/op         16 allocs/op
+//BenchmarkFact3_4-4       2504330               475 ns/op             400 B/op         20 allocs/op
+//BenchmarkFact3_5-4       1285077               940 ns/op             800 B/op         40 allocs/op
+func BenchmarkFact3_1(b *testing.B) {benchmarkFactorial3(1, b)}
+func BenchmarkFact3_2(b *testing.B) {benchmarkFactorial3(5, b)}
+func BenchmarkFact3_3(b *testing.B) {benchmarkFactorial3(8, b)}
+func BenchmarkFact3_4(b *testing.B) {benchmarkFactorial3(10, b)}
+func BenchmarkFact3_5(b *testing.B) {benchmarkFactorial3(20, b)}
+
+// 循环实现
+//BenchmarkFact4_1-4      483686618                2.29 ns/op            0 B/op          0 allocs/op
+//BenchmarkFact4_2-4      349090507                3.51 ns/op            0 B/op          0 allocs/op
+//BenchmarkFact4_3-4      272061386                4.51 ns/op            0 B/op          0 allocs/op
+//BenchmarkFact4_4-4      244875709                4.86 ns/op            0 B/op          0 allocs/op
+//BenchmarkFact4_5-4      143601668                8.20 ns/op            0 B/op          0 allocs/op
+func BenchmarkFact4_1(b *testing.B) {benchmarkFactorial4(1, b)}
+func BenchmarkFact4_2(b *testing.B) {benchmarkFactorial4(5, b)}
+func BenchmarkFact4_3(b *testing.B) {benchmarkFactorial4(8, b)}
+func BenchmarkFact4_4(b *testing.B) {benchmarkFactorial4(10, b)}
+func BenchmarkFact4_5(b *testing.B) {benchmarkFactorial4(20, b)}
+
 func benchmarkFactorial1(n int, b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
@@ -62,5 +95,19 @@ func benchmarkFactorial2(n int, b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		Factorial2(n, 1)
+	}
+}
+
+func benchmarkFactorial3(n int, b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		Factorial3(n)
+	}
+}
+
+func benchmarkFactorial4(n int, b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		Factorial4(n)
 	}
 }
